9ccd9a9a2fe469c072f7686dcca4166c93c899b0
Dmitry-creator <78728623+Dmitry-creator@users.noreply.github.com>
2026-04-22 20:24:55 +0300

    Game v1.1
    Добавлен разворот персонажа в зависимости от его движения

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,9 @@ public class Player : MonoBehaviour
     // Ключ для обработки действия персонажа стоит он на месте или бежит
     private bool isRunning = false;
 
+    // Переменная для обработки действия персонажа для считывания в какую сторону он бежит
+    private string isDirection = "";
+
     private void Awake()
     {
         // Создаем единственный экземпляр класса. this ссылка на текущий экземпляр класса в котором выполняется код
@@ -35,7 +38,18 @@ public class Player : MonoBehaviour
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
         inputVector = inputVector.normalized;
         rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
-        //Debug.Log(inputVector);
+        Debug.Log(inputVector);
+
+        // Считывание и запись в переменную того куда персонаж бежит
+        if (inputVector.x == 1.0)
+        {
+            isDirection = "Right";
+        }
+        if (inputVector.x == -1.0)
+        {
+            isDirection = "Left";
+        }
+
         // Проверяем бежит персонаж или стоит на месте
         if (Mathf.Abs(inputVector.x) > minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed)
         {
@@ -46,8 +60,15 @@ public class Player : MonoBehaviour
         }
     }
 
+    // Функция возвращает лож-персонаж стоит истина-персонаж бежит
     public bool IsRunning()
     {
         return isRunning;
+    }
+
+    // Функция возвращает направления движения персонажа
+    public string IsDirection()
+    {
+        return isDirection;
     }
 }
--- Assets/Scripts/PlayerVisual.cs
+++ Assets/Scripts/PlayerVisual.cs
@@ -4,7 +4,8 @@ public class PlayerVisual : MonoBehaviour
 {
     // Получаем доступ к аниматору для получения параметра IsRunning
     private Animator animator;
-
+    // Получаем доступ к cпрайту для дальнейшей обработки
+    private SpriteRenderer spriteRenderer;
     // Константа для переменной IS_RUNNING в аниматоре
     private const string IS_RUNNING = "IsRunning";
 
@@ -13,11 +14,22 @@ public class PlayerVisual : MonoBehaviour
     {
         // Инициализация аниматора
         animator = GetComponent<Animator>();
+
+        // Инициализация спрайта
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         // Обновление переменной в аниматоре для использования другой анимации
         animator.SetBool(IS_RUNNING, Player.Instance.IsRunning());
+        if (Player.Instance.IsDirection() == "Left")
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }
